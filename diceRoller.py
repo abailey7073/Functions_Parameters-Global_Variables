bf65bc96038d59253b3e94dcd6e835624f8708c3
abailey7073 <abailey7073@sohamcollege.org.uk>
2014-05-15 09:42:50 +0100

    abailey7073

--- diceRoller.py
+++ diceRoller.py
@@ -1,7 +1,8 @@
-#Coin flip program
+#Dice roll program
 #Describe the purpose of this program here.
 
-inport random;time
+import random
+import time
 
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
@@ -11,24 +12,25 @@ s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 
 def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
-
-
-def show_dice(roll)
-    if roll = 1:
-        print(S1)
-    else roll = 2
+    print("rolling.....")
+    roll = random.randint(1,7)
+    show_dice(roll)
+    
+def show_dice(roll):
+    if roll == 1:
+        print(s1)
+    if roll == 2:
         print(s2)
-    else roll = 3
+    if roll == 3:
         print(s3)
-    else roll = 4
+    if roll == 4:
         print(s4)
-    else roll = 5
+    if roll == 5:
         print(s5)
-    else roll = 6
+    if roll == 6:
         print(s6)
-
-roll
+    print(roll)
+        
+roll()
 time.sleep(1)
 show_dice(roll)
